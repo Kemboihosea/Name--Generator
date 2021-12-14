abd24aa8795d3fc4347e0103fef94ebab1fb4026
kemboihosea <kemboihosea98@gmail.com>
2021-12-14 08:54:02 +0300

    commit 3

---
+++ moringa.js
@@ -0,0 +1,22 @@
+moringa.js
+//function getAkanName() {
+    let yearofbirth=document.getElementById("year-input")Value;
+    let monthofbirth=Number(document.getElementById("month-input")Value;
+    let gender=document.getElementsByValue("gender");
+    
+    // function getgender
+    function getgender() {
+        if (gender.cleared){ 
+            return gender.Value;
+        }
+    }
+}
+let mygendervalue=getgender();
+// function get gender by value
+if (monthofbirth<0 monthofbirth >12){
+    return true;
+}else{
+    return false;
+}
+}
+
